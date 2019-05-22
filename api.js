7490a5dffc0c63b25e55b0b102ca7d2c1da0dae3
Sachin <sachindangol@gmail.com>
2019-05-22 17:55:26 +0800

    working copy of get issues and update issues

--- api.js
+++ api.js
@@ -1,47 +1,90 @@
+"use strict";
 let commander = require('commander');
 let request = require('request');
-const BASEURL = 'http://demo.redmine.org';
-
-const REDMINE_SERVER = '';
+const CONFIG = require('./config.json');
+const BASEURL = CONFIG.redmine.baseAPIEndpoint;
 
 commander
 .usage('OPTIONS:')
-.option('--username <username>', 'redmine username defaults to hard coded value')
-.option('--password <password>', 'redmine password defaults to hard coded value')
+.option('--key <key>', 'redmine api key defaults to hard coded value')
 .parse(process.argv);
 
-const USERNAME = commander.username ? commander.username : 'test';
-const PASSWORD = commander.password ? commander.password : 'test123';
+const KEY = commander.key ? commander.key : CONFIG.redmine.APIKey; // super_user cred superuser123
+
+const HEADERS = {
+    'Content-Type': 'application/json',
+    'X-Redmine-API-Key': KEY,
+};
+
+// status_id 2 = 'In Progress'
 // status_id 3 = resolved
+// status_id 4 = 'Feature'
+// status_id 5 = 'Closed'
 
-
-console.log(`${USERNAME} username -> ${PASSWORD} password`);
-request(`${BASEURL}/issues.json?status_id=1`, (err, res, body) => {
-    if(err) {return console.log(err)}
-
-    let bodyObj = JSON.parse(body);
-
-    bodyObj.issues.forEach((e, i) => {
-        //console.log(e); 
-        if(e.id === 253436) {
-           // console.log(e);
-            request.put(
-                {
-                    url: `${BASEURL}/issues/${e.id}.json`, 
-                    json: {
-                        "issue": {
-                            "status_id": 3,
-                            "subject": "bye bye"
-                        }
-                    }
-                }, 
-                (err, res, body) => {
-                    if(err) {console.log(err)};
-                    console.log(body);
+// sprint parent taskids 991,989,979,978,965,964,945,944,911,910,891,890,853,852,810,809,757,741,739,738,719,681,663,656
+[991,989].forEach(
+//close all the sprint parent tasks    
+    (issueID, i) => {
+        update_issues(
+            `${BASEURL}/issues.json?issue_id=${issueID}`,
+            {
+                "issue": {
+                    "status_id": 5
                 }
-            );
-        }
-    });
+            }
+        );
+    }
+);
 
-    //console.log(JSON.stringify(JSON.parse(body), null, 4));
-});
+/*
+update_issues:
+    Finds the issue and update the issues one by one.
+ARGS:
+    issuesUrl: api endpoint to filter out or list the issues - get issues endpoint
+    updateJSON: update JSON - json containing th fields you want to update - this will be passed to issues update put endpoint
+*/
+async function update_issues(issuesUrl, updateJSON) {
+    let issues = await get_issues(issuesUrl);
+    issues.forEach((issue, i) => {
+        console.log("Updating " + issue.id + '...');
+        //            console.log(i, '--->', issue);
+        request({
+                url: `${BASEURL}/issues/${issue.id}.json`,
+                headers: HEADERS,
+                method: 'put',
+                json: updateJSON
+            },
+            (err, res, body) => {
+                if (err) {
+                    console.log(err)
+                };
+                //console.log(JSON.stringify(res));
+                if (res.statusCode === 200) {
+                    console.log("updated " + issue.id + " successfully");
+                } else {
+                    console.log("Failed to update " + issue.id + " Error: " + res.statusCode + ":" + res.headers.status);
+                }
+            }
+        );
+
+    });
+}
+
+function get_issues(url) {
+    return new Promise(
+        (resolve, reject) => {
+        request({
+            url: url,
+            headers: HEADERS
+            },
+            (err, res, body) => {
+                if(err) {
+                    console.log(err)
+                    reject(err);
+                }
+                let bodyObj = JSON.parse(body);
+                let issues = bodyObj['issues'];
+                resolve(issues);
+        });
+    });
+}
